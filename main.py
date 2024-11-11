1f5f726e9ade8b11bfa6c458068fcfb68efbb762
g-sillva <silvagabriel.limars@gmail.com>
2024-11-11 17:35:19 -0300

    -

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ def main():
     # Getting all the ips in the network based
     network_ips = get_ips_by_input()
 
-    print(network_ips)
+    # Store icmp results
+    icmp_results = {}
 
-    pass
-
+    for ip in network_ips:
+        print(f"Scanning {ip}")
+        # create icmp packet
+        # send icmp packet
+        # store result
+        pass
+    
 
 if __name__ == '__main__':
     main()
