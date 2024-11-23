88d4048d0a2e77166be462f8a64e865981a34f08
João Vicente Campanher <jvcampanher@gmail.com>
2024-11-23 11:46:02 -0300

    ip for linux

--- e1/icmp.py
+++ e1/icmp.py
@@ -25,7 +25,7 @@ def send_ping(sock, src_addr, dest_addr, identifier):
     """Send an ICMP packet"""
     icmp_packet = create_icmp_packet(identifier)
     print(platform.system().lower())
-    if platform.system().lower() == 'windows':
+    if platform.system().lower() == 'windows' or platform.system().lower() == 'linux':
         ip_header = create_ip_header(src_addr, dest_addr)
         packet = ip_header + icmp_packet
         sock.sendto(packet, (dest_addr, 0))
