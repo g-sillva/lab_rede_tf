32aa69c695f1cb54a4906c036587ed2c7da5ebfc
João Vicente Campanher <jvcampanher@gmail.com>
2024-11-23 13:10:57 -0300

    revert

--- checksum.py
+++ checksum.py
@@ -1,23 +1,21 @@
 def checksum(source_string):
-    """Calculate the checksum of the given data."""
+    """Calcula o checksum do cabeçalho"""
     sum = 0
     count_to = (len(source_string) // 2) * 2
     count = 0
-
-    # Sum all 16-bit words
     while count < count_to:
-        this_val = source_string[count] + (source_string[count + 1] << 8)
-        sum += this_val
-        sum &= 0xffffffff  # Keep it 32-bit
-        count += 2
+        this_val = source_string[count + 1] * 256 + source_string[count]
+        sum = sum + this_val
+        sum = sum & 0xffffffff
+        count = count + 2
 
-    # Handle the last byte, if applicable
     if count_to < len(source_string):
-        sum += source_string[-1]
-        sum &= 0xffffffff
+        sum = sum + source_string[-1]
+        sum = sum & 0xffffffff
 
-    # Fold to 16 bits and complement
     sum = (sum >> 16) + (sum & 0xffff)
-    sum += (sum >> 16)
+    sum = sum + (sum >> 16)
     answer = ~sum
-    return answer & 0xffff  # Return only 16 bits
+    answer = answer & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+    return answer
--- e1/icmp.py
+++ e1/icmp.py
@@ -14,10 +14,10 @@ ICMP_ECHO_REPLY = 0
 
 def create_icmp_packet(identifier):
     """Create an ICMP packet"""
-    header = struct.pack("!BBHHH", ICMP_ECHO_REQUEST, 0, 0, identifier, 1)
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, 0, identifier, 1)
     data = b'Ping!' + (192 * b'Q')
     checksum_value = checksum(header + data)
-    header = struct.pack("!BBHHH", ICMP_ECHO_REQUEST, 0,
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0,
                          checksum_value, identifier, 1)
     return header + data
 
