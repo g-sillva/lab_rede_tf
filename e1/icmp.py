24319b6abaf7edb916a6f3a91c1854f50e408d7c
João Vicente Campanher <jvcampanher@gmail.com>
2024-11-23 14:20:16 -0300

    format

--- e1/icmp.py
+++ e1/icmp.py
@@ -14,10 +14,11 @@ ICMP_ECHO_REPLY = 0
 
 def create_icmp_packet(identifier):
     """Create an ICMP packet"""
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, 0, identifier, 1)
+    header = struct.pack("!BBHHH", ICMP_ECHO_REQUEST,
+                         ICMP_ECHO_REPLY, 0, identifier, 1)
     data = b'Ping!'
     checksum_value = checksum(header + data)
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0,
+    header = struct.pack("!BBHHH", ICMP_ECHO_REQUEST, ICMP_ECHO_REPLY,
                          checksum_value, identifier, 1)
     return header + data
 
