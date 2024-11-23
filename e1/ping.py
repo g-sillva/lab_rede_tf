4e16542c0416a92f6b47f869ef91eb7299946874
João Vicente Campanher <jvcampanher@gmail.com>
2024-11-23 14:07:51 -0300

    get correct ip

--- e1/ping.py
+++ e1/ping.py
@@ -5,20 +5,29 @@ import platform
 
 from e1.icmp import send_ping, receive_ping
 
+def get_local_ip():
+    """Get the local IP address of the machine"""
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        try:
+            s.connect(("8.8.8.8", 80))
+            local_ip = s.getsockname()[0]
+        except Exception:
+            local_ip = "127.0.0.1"
+    return local_ip
 
 def ping(dest_addr):
     """Main function to send and receive ICMP packets"""
     try:
-        # if (platform.system().lower() == 'linux'):
-        #     sock = socket.socket(
-        #         socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
-        #     sock.bind(('eth0', 0))
+        if (platform.system().lower() == 'linux'):
+            sock = socket.socket(
+                socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
+            sock.bind(('eth0', 0))
 
-        # elif (platform.system().lower() == 'windows'):
-        #     sock = socket.socket(
-        #         socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-        #     sock.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-        # else:
+        elif (platform.system().lower() == 'windows'):
+            sock = socket.socket(
+                socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+            sock.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+        else:
             sock = socket.socket(
                 socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
 
@@ -26,7 +35,7 @@ def ping(dest_addr):
         print("Permission denied. Try running as root.")
         return None
 
-    src_addr = socket.gethostbyname(socket.gethostname())
+    src_addr = get_local_ip()
     identifier = os.getpid() & 0xFFFF
     send_ping(sock, src_addr, dest_addr, identifier)
 
